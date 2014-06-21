6edbabe2c6e2f4aa9a8ca6d8c082fad6541883fc
Ragora <ragoradev@gmail.com>
2014-06-21 18:03:37 -0400

    Updated to use new initialize command

--- fuzzball.py
+++ fuzzball.py
@@ -24,7 +24,7 @@ class Modification:
 
 	commands = None
 
-	def __init__(self, **kwargs):
+	def initialize(self, **kwargs):
 		self.config = kwargs['config']
 		self.interface = kwargs['interface']
 		self.session = kwargs['session']
